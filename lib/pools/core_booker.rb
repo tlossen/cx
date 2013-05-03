8972a04683c191a53ad395679c2808f93a198373
Tim Lossen <tim@lossen.de>
2013-05-03 23:32:20 +0200

    added core_booker pool

---
+++ lib/pools/core_booker.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+class CoreBooker < Pool
+
+  def worker_body
+    forever do
+      return if stop_requested
+      trade_id = $redis.rpop("trades")
+      if trade_id
+        TradeBook.new(trade_id: trade_id).execute
+      else
+        sleep(0.1)
+      end
+    end
+  end
+
+end
